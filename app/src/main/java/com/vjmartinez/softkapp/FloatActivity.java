45164ec7670325977a489402076b6e7811e94988
vjmartinez89 <vijumaba89@gmail.com>
2018-11-05 09:43:22 -0500

    Initial commit

---
+++ app/src/main/java/com/vjmartinez/softkapp/FloatActivity.java
@@ -0,0 +1,15 @@
+package com.vjmartinez.softkapp;
+
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+public class FloatActivity extends AppCompatActivity {
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.float_activity);
+    }
+}
